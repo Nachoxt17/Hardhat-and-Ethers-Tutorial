4afbdb581d95f43a24559ceb06fc458f9cfc1893
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-04-26 17:01:42 -0300

    Finished Tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
+/**+-Ethers.js Documentation:_ https://docs.ethers.io/v5/getting-started/ .*/
 import { useState } from "react";
 import { ethers } from "ethers";
-import logo from "./logo.svg";
 import "./App.css";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  const [greeting, setGreetingValue] = useState("");
+
+  //+-Connect with Metamask Ethereum Account to make a transaction:_
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  //+-Connect with Metamask Extension:_
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log("data: ", data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  //+-Change things in the DApp and Process Transactions:_
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider });
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue("");
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+          value={greeting}
+        />
       </header>
     </div>
   );
